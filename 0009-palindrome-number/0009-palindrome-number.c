3d23187362507bbf1ab93e2a4cc416de3b949cbc
Harsh <harsh121600@gmail.com>
2026-01-16 11:28:41 +0530

    Time: 4 ms (25.49%), Space: 8.1 MB (81.66%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,19 @@
+#include <stdbool.h>
+
+bool isPalindrome(int x) {
+    // Negative numbers and numbers ending in 0 (except 0) are not palindromes
+    if (x < 0 || (x % 10 == 0 && x != 0)) {
+        return false;
+    }
+
+    int rev = 0;
+
+    while (x > rev) {
+        rev = rev * 10 + x % 10;
+        x /= 10;
+    }
+
+    // For even digits: x == rev
+    // For odd digits: x == rev / 10
+    return (x == rev || x == rev / 10);
+}
