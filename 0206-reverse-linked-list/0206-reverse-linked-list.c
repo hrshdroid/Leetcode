8b21ecdf75f8d4f2c35c4785e0dad47700e1d6e8
Harsh <harsh121600@gmail.com>
2026-03-25 22:21:45 +0530

    Time: 0 ms (100%), Space: 11.5 MB (12.16%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.c
@@ -0,0 +1,14 @@
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode* temp = head;
+    struct ListNode* prev = NULL;
+    struct ListNode* next = NULL;
+
+    while (temp != NULL) {
+        next = temp->next;
+        temp->next = prev;
+        prev = temp;
+        temp = next;
+    }
+
+    return prev;
+}
