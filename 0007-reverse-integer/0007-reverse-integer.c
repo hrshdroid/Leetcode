36ddd8e08ea731ea6bf54478a4a52c907436bb8e
Harsh <harsh121600@gmail.com>
2026-03-28 15:28:06 +0530

    Time: 6 ms (22.81%), Space: 9.3 MB (24.97%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.c
@@ -0,0 +1,13 @@
+int reverse(int x) {
+    long res = 0;
+
+    while (x != 0) {
+        res = res * 10 + x % 10;
+        x /= 10;
+
+        if (res > 2147483647 || res < -2147483648)
+            return 0;
+    }
+
+    return (int)res;
+}
